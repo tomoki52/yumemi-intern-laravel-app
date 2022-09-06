e225b599fe32eea6ba17b917b57e5de1b8a35d80
小西 智樹 <>
2022-09-06 15:36:17 +0900

    テストデータにパスワードを追加

--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -3,8 +3,7 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Support\Facades\Hash;
+
 use Tests\TestCase;
 
 class UserControllerTest extends TestCase
@@ -15,19 +14,24 @@ class UserControllerTest extends TestCase
      *
      * @return void
      */
-    use WithFaker;
-    public function test_example()
+    public function test_user_create()
     {
         $test_data = [
             'name' => 'k_nakano',
             'email' => 'k_nakano@example.com',
+            'password' => 'password',
+        ];
+        $expected = [
+            'name' => 'k_nakano',
+            'email' => 'k_nakano@example.com',
         ];
         $response = $this->postJson('/api/user', $test_data);
-        $this->assertDatabaseHas('users', $test_data);
+        $this->assertDatabaseHas('users', $expected);
         $response->assertExactJson(
             [
                 'message' => 'User record created',
             ]);
+
         $response->assertStatus(200);
 
     }
