d383ac941ba2311119277f28485ee9498fec1300
小西 智樹 <>
2022-09-07 13:49:18 +0900

    ステータスコードを定数に変更　ログイン時失敗処理を401に変更　ログインテストの追加　openapi.yamlの更新

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use http\Env\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-
+use Symfony\Component\HttpFoundation\Response as ResponseCode;
 class UserController extends Controller
 {
     //
@@ -17,31 +18,31 @@ class UserController extends Controller
             'password' => Hash::make($request->input('password')),
         ]);
         return response()->json([
-                'message' => 'User record created',
+
             ],
-            200);
+            ResponseCode::HTTP_OK);
     }
     public function  loginUser(Request $request){
         $user = User::where('email',$request->email)->first();
         if(empty($user)) {
             return response()->json([
-                'message' => 'user is not found.'
+
             ],
-                404);
+                ResponseCode::HTTP_UNAUTHORIZED);
         }
 
         if(Hash::check($request->password,$user->password)){
             $token = $user->createToken($request->email);
             return response()->json([
-                'message'=> 'login is successful.',
+
                 'token' => $token->plainTextToken,
             ],
-            200);
+                ResponseCode::HTTP_OK);
         }else{
             return response()->json([
-                'message'=>'password is wrong.'
+
             ],
-            400);
+                ResponseCode::HTTP_UNAUTHORIZED);
         }
 
     }
