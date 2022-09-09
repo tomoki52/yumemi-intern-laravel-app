14e00ba72eea6d2cf8e91965b90068ad1f4a1e5c
小西 智樹 <>
2022-09-09 18:27:19 +0900

    Companyにルーティングとmodel追加

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class Company extends Model
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'profile',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+    ];
 }
--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,5 @@ Route::get('/first', \App\Http\Controllers\GetFirstController::class);
 Route::get('/company', \App\Http\Controllers\GetCompanyController::class);
 Route::post('/user', '\App\Http\Controllers\UserController@createUser');
 Route::post('/user/login','\App\Http\Controllers\UserController@loginUser');
+Route::post('/company', '\App\Http\Controllers\CompanyController@createCompany');
+Route::post('/company/login','\App\Http\Controllers\CompanyController@loginCompany');
