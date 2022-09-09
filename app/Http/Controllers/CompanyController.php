f260c4aba50b19aa71e276dcb9f93725598c8419
小西 智樹 <>
2022-09-09 18:21:21 +0900

    企業のアカウント作成とログイン機能追加

---
+++ app/Http/Controllers/CompanyController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Company;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response as ResponseCode;
+
+class CompanyController extends Controller
+{
+    //
+    public function createCompany(Request $request){
+
+        $company = Company::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+            'profile' => $request->input('profile'),
+        ]);
+        return response()->json([
+
+        ],
+            ResponseCode::HTTP_OK);
+    }
+    public function  loginCompany(Request $request){
+        $company = Company::where('email',$request->email)->first();
+        if(empty($company)) {
+            return response()->json([
+
+            ],
+                ResponseCode::HTTP_UNAUTHORIZED);
+        }
+
+        if(Hash::check($request->password,$company->password)){
+            $token = $company->createToken($request->email);
+            return response()->json([
+
+                'token' => $token->plainTextToken,
+            ],
+                ResponseCode::HTTP_OK);
+        }else{
+            return response()->json([
+
+            ],
+                ResponseCode::HTTP_UNAUTHORIZED);
+        }
+
+    }
+}
---
+++ tests/Feature/CompanyControllerTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Spectator\Spectator;
+use Symfony\Component\HttpFoundation\Response as ResponseCode;
+use Tests\TestCase;
+
+class CompanyControllerTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    use RefreshDatabase;
+
+    public function test_company_create()
+    {
+        //Spectator::using('openapi.yaml');
+        $test_data = [
+            'name' => 'yumemi',
+            'email' => 'yumemi@example.com',
+            'password' => 'password',
+            'profile' => 'sample profile',
+        ];
+        $expected = [
+
+            'name' => 'yumemi',
+            'email' => 'yumemi@example.com',
+            'profile' => 'sample profile',
+        ];
+
+        $response = $this->postJson('/api/company', $test_data);
+        $this->assertDatabaseHas('companies', $expected);
+
+        $response
+            //->assertValidRequest()
+            ->assertExactJson([])
+            //->assertValidResponse(ResponseCode::HTTP_OK);
+            ->assertStatus(ResponseCode::HTTP_OK);
+    }
+
+    public function test_company_login()
+    {
+        //Spectator::using('openapi.yaml');
+
+        $test_create_data = [
+            'name' => 'yumemi',
+            'email' => 'yumemi@example.com',
+            'password' => 'password',
+            'profile' => 'sample profile',
+        ];
+
+        $test_login_data = [
+            'email' => 'yumemi@example.com',
+            'password' => 'password',
+        ];
+
+        $this->postJson('/api/company', $test_create_data);
+
+        $response = $this->postJson('/api/company/login', $test_login_data);
+        $response
+            //->assertValidRequest()
+            //->assertValidResponse(ResponseCode::HTTP_OK);
+            ->assertStatus(ResponseCode::HTTP_OK);
+    }
+}
