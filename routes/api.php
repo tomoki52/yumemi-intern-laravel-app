bf615a9da4bf93656f7c40628daeb4139bcd55f7
小西 智樹 <>
2022-09-12 10:41:58 +0900

    不要なファイルを削除

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/first', \App\Http\Controllers\GetFirstController::class);
-Route::get('/company', \App\Http\Controllers\GetCompanyController::class);
 Route::post('/user', '\App\Http\Controllers\UserController@createUser');
 Route::post('/user/login','\App\Http\Controllers\UserController@loginUser');
 Route::post('/company', '\App\Http\Controllers\CompanyController@createCompany');
